2272c2074185a1d2c1dbf849096daf821f123f64
t4t5u0 <ymmtryk0902@gmail.com>
2020-05-09 16:14:22 +0900

    画像を転送できるようにした

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import io
+import aiohttp
 import json
 
 import discord
@@ -7,6 +9,9 @@ json_open = open('./info.json', 'r')
 json_load = json.load(json_open)
 
 client = discord.Client()
+
+# to_send_channel = client.get_channel(json_load['channel_id'])
+
 @client.event
 async def on_ready():
     print('ログインしました')
@@ -19,9 +24,19 @@ async def on_message(message):
         return
     if str(message.channel.type) != 'private':
         return
+    to_send_channel = client.get_channel(json_load['channel_id'])    
+    print(f'message:{message}')
     text = message.content
-    to_send_channel = client.get_channel(json_load['channel_id'])
-    await to_send_channel.send(text)
-
+    if len(text):
+        await to_send_channel.send(text)
+    for image in message.attachments:
+        image_url = image.url
+        image_name = image.filename
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image_url) as resp:
+                if resp.status != 200:
+                    return await to_send_channel.send('画像を取得できませんでした')
+                data = io.BytesIO(await resp.read())
+                await to_send_channel.send(file=discord.File(data, image_name))
 
 client.run(json_load['token'])
