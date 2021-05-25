53a88ee8666e224c6d111a6cdbff73cf7fff62e5
t4t5u0 <ymmtryk0902@gmail.com>
2021-05-26 00:09:18 +0900

    [add] Cogで再実装

---
+++ cog.py
@@ -0,0 +1,58 @@
+import csv
+import io
+import json
+from typing import OrderedDict
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+
+class QuestionBotCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        with open('./info.json', 'r') as f:
+            self.json_load = json.load(f)
+        self.channel_id = int(self.json_load['channel_id'])
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        # DM限定
+        if str(message.channel.type) != 'private':
+            return
+        to_send_channel: discord.TextChannel = self.bot.get_channel(
+            self.channel_id)
+        text = message.content
+        if text:
+            with open('./store.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow([message.id, message.author, message.content])
+            await to_send_channel.send(text)
+        # 受信したファイルを取得する
+        for file_ in message.attachments:
+            file_url = file_.url
+            file_name = file_.filename
+            async with aiohttp.ClientSession() as session:
+                async with session.get(file_url) as resp:
+                    if resp.status != 200:
+                        return await to_send_channel.send('ファイルを取得できませんでした')
+                    data = io.BytesIO(await resp.read())
+                    # 送信部分
+                    await to_send_channel.send(file=discord.File(data, file_name))
+
+    @commands.command(name='set')
+    async def _set(self, ctx: commands.Context):
+        "質問を転送するチャンネルを設定します"
+        channel_id: int = ctx.channel.id
+        self.channel_id = channel_id
+        with open('./info.json', 'w') as f:
+            updated = json.load(f, object_pairs_hook=OrderedDict)
+            updated['channel_id'] = channel_id
+            json.dump(updated, f, indent=4, ensure_ascii=False)
+        await ctx.send('メッセージを転送するチャンネルをこのチャンネルに変更しました')
+
+
+def setup(bot: commands.Bot):
+    return bot.add_cog(QuestionBotCog(bot))
--- main.py
+++ main.py
@@ -3,45 +3,93 @@ import json
 import csv
 
 import aiohttp
+from pathlib import Path
+import configparser
+
 import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
 
-json_open = open('./info.json', 'r')
-json_load = json.load(json_open)
 
-client = discord.Client()
+class UserHelp(commands.DefaultHelpCommand):
+    def __init__(self):
+        super().__init__()
+        self.commands_heading = 'コマンド: '
+        self.no_category = 'other'
+        self.command_attrs['help'] = 'コマンド一覧と簡単な説明を表示'
 
-@client.event
-async def on_ready():
-    # print('ログインしました')
-    # print('-'*40)
-    pass
+    def command_not_found(self, string: str):
+        return f'{string} というコマンドは見つかりませんでした'
 
-# DMにメッセージが送信されたら、テキストや画像を取得して
-# channel に send する
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if str(message.channel.type) != 'private':
-        return
-    to_send_channel = client.get_channel(json_load['channel_id'])    
-    # print(f'message:{message}')
-    text = message.content
-    if len(text):
-        with open('./store.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow([message.id ,message.author ,message.content])
-        await to_send_channel.send(text)
-    # 受信したファイルを取得する
-    for file_ in message.attachments:
-        file_url = file_.url
-        file_name = file_.filename
-        async with aiohttp.ClientSession() as session:
-            async with session.get(file_url) as resp:
-                if resp.status != 200:
-                    return await to_send_channel.send('ファイルを取得できませんでした')
-                data = io.BytesIO(await resp.read())
-                # 送信部分
-                await to_send_channel.send(file=discord.File(data, file_name))
+    def get_ending_note(self):
+        return ("質問箱Bot")
 
-client.run(json_load['token'])
+
+# path = Path.cwd().glob('**/config.ini')
+# config = configparser.ConfigParser()
+# config.read(path)
+# TOKEN = config['TOKEN']['token']
+
+# with open('./info.json', 'r') as f:
+#     json_load = json.load(f)
+# TOKEN = json_load['token']
+
+# prefix = '/'
+# bot = Bot(command_prefix=prefix, help_command=UserHelp())
+# bot.load_extension('cog.arxiv_check')
+
+
+def main():
+    with open('./info.json', 'r') as f:
+        json_load = json.load(f)
+    TOKEN = json_load['token']
+
+    prefix = '/'
+    bot = Bot(command_prefix=prefix, help_command=UserHelp())
+    bot.load_extension('cog')
+    bot.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
+
+
+# json_open = open('./info.json', 'r')
+# json_load = json.load(json_open)
+
+# client = discord.Client()
+
+# @client.event
+# async def on_ready():
+#     # print('ログインしました')
+#     # print('-'*40)
+#     pass
+
+# # DMにメッセージが送信されたら、テキストや画像を取得して
+# # channel に send する
+# @client.event
+# async def on_message(message):
+#     if message.author.bot:
+#         return
+#     if str(message.channel.type) != 'private':
+#         return
+#     to_send_channel = client.get_channel(json_load['channel_id'])    
+#     # print(f'message:{message}')
+#     text = message.content
+#     if len(text):
+#         with open('./store.csv', 'a') as f:
+#             writer = csv.writer(f)
+#             writer.writerow([message.id ,message.author ,message.content])
+#         await to_send_channel.send(text)
+#     # 受信したファイルを取得する
+#     for file_ in message.attachments:
+#         file_url = file_.url
+#         file_name = file_.filename
+#         async with aiohttp.ClientSession() as session:
+#             async with session.get(file_url) as resp:
+#                 if resp.status != 200:
+#                     return await to_send_channel.send('ファイルを取得できませんでした')
+#                 data = io.BytesIO(await resp.read())
+#                 # 送信部分
+#                 await to_send_channel.send(file=discord.File(data, file_name))
+
+# client.run(json_load['token'])
